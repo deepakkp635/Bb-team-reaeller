e2a10057190a79a47f18a419e0012cea30f3d412
deepakkp635 <deepakkp635@gmail.com>
2026-05-30 08:54:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require("express");
 
+// 👉 TOKEN Render Environment से आएगा
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, "Hello 😎 Bot working on Render!");
+// 👉 simple reply
+bot.on("message", (msg) => {
+  bot.sendMessage(msg.chat.id, "Hello 👋 Bot chal raha hai!");
+});
+
+// 👉 Render ke liye server (IMPORTANT)
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
 });
